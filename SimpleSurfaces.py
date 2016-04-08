d9d99c3ff526d32d5bc59ff5ea0d79c3cbaa6e72
Simon Oldfield <soldfield@users.noreply.github.com>
2016-04-08 13:18:00 +0100

    Update SimpleSurfaces.py

--- SimpleSurfaces.py
+++ SimpleSurfaces.py
@@ -6,7 +6,23 @@ Short script to generate a two-dimensional fold, based on a cosine curve.
 
 Outputs xyz point cloud as a text file in the working directory.
 
-@author: S. Oldfield
+@author: Simon J. Oldfield
+
+   Copyright 2016 Simon Oldfield
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+   
 """
 
 ## Initially attemting to build a simple sin wave
