04046bee0016ae46d581db6313ed665ae5fd1200
Simon Oldfield <soldfield@users.noreply.github.com>
2016-04-08 13:17:36 +0100

    Update Monocline_xyz.py

--- Monocline_xyz.py
+++ Monocline_xyz.py
@@ -4,7 +4,23 @@ Created on Wed Nov 11 15:05:58 2015
 
 Script to generate simple monoclinal surfaces as point clouds.
 
-@author: ee10sjo
+@author: Simon J Oldfield
+
+   Copyright 2016 Simon Oldfield
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+   
 """
 
 import numpy as np
